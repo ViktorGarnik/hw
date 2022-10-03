87a0c7acdb9b02cc0325fd26bb9552eda892e1d1
ViktorGarnik <garnik_viktor@mail.ru>
2022-10-03 20:36:01 +0300

    commit

---
+++ hw_1.py
@@ -0,0 +1,123 @@
+#Task 1
+
+a = 3
+b = 4
+c = 5
+print(a+b+c)
+
+a = 5
+b = 1
+c = 2
+print(a-b+c)
+
+a = 2
+b = 3
+c = 4
+print(a*b//c)
+
+a = 2
+b = 3
+c = 2
+print(a*b%c)
+
+a = 4
+b = 4
+c = 5
+print((a**b/2)-c*3)
+
+#Task 2
+
+import math
+a = 6
+b = 4
+area = (a * b) / 2
+c = (a**2  + b**2) ** 0,5
+print('area', c)
+
+#Task str
+
+a = 'Hello World'
+result = a.count(" ") + 1
+print(str(result))
+
+# a = ' Hello   World '
+# a_list = a.split()
+# print(str(len(a_list)))
+
+#Task str 2
+
+a = 'Hello'
+print(a[3])
+print(a[-2])
+print(a[0:5])
+print(a[0:-2])
+print(a[::2])
+print(a[1::2])
+print(a[::-2])
+print(len(a))
+
+#Task str 3
+
+my_str = 'wertyhgfhhdjjh'
+a = my_str.replace('h', 'H', 3)
+print(a)
+
+#Task namber 1
+
+a = 200
+x = str(a)
+print(x[-1])
+
+a = 123
+x = str(a)
+print(x[-1])
+
+a = 587
+x = str(a)
+print(x[-1])
+
+#Task number 2
+
+a = 12
+print(a // 10)
+a = 58
+print(a // 10%10)
+a = 123
+print(a // 10%10)
+a = 5839
+print(a // 10%10)
+a = 7
+print(a // 10%10)
+a = 4
+print(a // 10%10)
+
+#Task number 3
+
+a = 123
+b = str(a)
+c = list(b)
+d = map(int, c)
+s = sum(d)
+print(s)
+
+a = 545
+b = str(a)
+c = list(b)
+d = map(int, c)
+s = sum(d)
+print(s)
+
+a = 999
+b = str(a)
+c = list(b)
+d = map(int, c)
+s = sum(d)
+print(s)
+
+a = 782
+b = str(a)
+c = list(b)
+d = map(int, c)
+s = sum(d)
+print(s)
+
